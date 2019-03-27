282dc47992bb35893563c6f3daa4d3b6da44a5b5
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2019-03-27 22:33:53 +0100

    Update new 1.js
    Tip : change 200 to 100 for more OP effects ( line 34 )

--- vB cy/new 1.js
+++ vB cy/new 1.js
@@ -9,13 +9,9 @@ var a = q.replace(/"/g,"").toLowerCase()
 inp.value=(text2num(a))
 si.click()
 go.click()
-//localStorage.setItem('po',localStorage.getItem('po')+1)
-//if(document.querySelector('#bee_complete').style.visibility!="hidden"){document.querySelector('#bee_complete > div.actions > button').click()}
 }
 
 function text2num(a){
-//	var cd=5
-//if(localStorage.getItem('po')>=150){localStorage.setItem('po',0);return 7878;}
 switch(a){
 case 'com': return "compromise"; break;
 case 'bac': return "bacteria"; break;
@@ -38,10 +34,3 @@ case 'dec': return "declaration"; break;
 setInterval(lol,200)
 setTimeout(function(){document.querySelector('#bee_complete > div.actions > button').click()},5000)
 window.confirm=function(){return true;}
-//window.open('javascript:window.confirm= function(){return true;}','_top')
-var s = document.createElement('script');
-s.innerHTML = "window.confirm= function(){return true;}"
-document.body.appendChild(s);
-document.body.insertAdjacentHTML('afterbegin','<div id="over" style="background-color: black; color:white; z-index: 999999;display:block;height:100%;top: 0; left: 0;right: 0;bottom: 0;position:fixed"></div>');
-document.getElementById('over').innerText=document.getElementsByClassName('points')[0].innerText
-document.body.style.display="none"
